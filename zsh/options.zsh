c84921600b977c83ca8c17bc2f0a21661dd6c19b
Pete Hicks <phicks@ripple.com>
2020-03-13 10:21:57 -0700

    adds no flow control and less options

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -1,4 +1,18 @@
 setopt autocd
 setopt histignorealldups
+setopt histignorespace
 setopt sharehistory
 setopt inc_append_history
+setopt no_flow_control
+
+READNULLCMD=less
+# This affects every invocation of `less`.
+#
+#   -i   case-insensitive search unless search string contains uppercase letters
+#   -R   color
+#   -F   exit if there is less than one page of content
+#   -X   keep content on screen after exit
+#   -M   show more info at the bottom prompt line
+#   -x4  tabs are 4 instead of 8
+export LESS=-iRXMx4
+
