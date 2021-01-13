1ddd6987c5f668801da6e9732a44043490aa7fde
pete hicks <petong@jah.net>
2021-01-13 15:23:52 -0800

    adds hotkey to move window to another monitor

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -43,3 +43,14 @@ caffeinateWatcher:start()
 hs.loadSpoon("Caffeine")
 --spoon.Caffeine:bindHotkeys({toggle = {hyper, "C"},})
 spoon.Caffeine:start()
+
+-- bind hotkey
+hs.hotkey.bind({'shift', 'cmd'}, '1', function()
+  -- get the focused window
+  local win = hs.window.focusedWindow()
+  -- get the screen where the focused window is displayed, a.k.a. current screen
+  local screen = win:screen()
+  -- compute the unitRect of the focused window relative to the current screen
+  -- and move the window to the next screen setting the same unitRect 
+  win:move(win:frame():toUnitRect(screen:frame()), screen:next(), true, 0)
+end)
