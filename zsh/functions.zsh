c1d6bebcec4c4521d38b5b4290704de296c68762
Pete Hicks <jph@jah.net>
2022-05-27 14:20:29 -0700

    adds kcc

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -12,4 +12,32 @@ ssh-reagent () {
   echo Cannot find ssh agent - maybe you should reconnect and forward it?
 }
 
+kcc() {
+  kubectl config get-contexts -o name | \
+    fzf --bind "enter:execute(kubectl config use-context {} &&  kubectl config view --flatten --minify > ~/.kube/{}.yaml && export KUBECONFIG=~/.kube/{}.yaml)" --bind "enter:+accept";
+}
 
+kns() {
+  kubectl get namespaces -o=jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' | fzf \
+    --bind "enter:execute(kubectl config set-context --current --namespace={} )" --bind "enter:+accept" \
+    --bind "ctrl-\:execute(kubectl get {+} -o yaml | nvim )" \
+
+}
+
+kd() {
+  #kubectl get all -o name | \
+  # kubectl describe --namespace $(kubectl get all --all-namespaces --template '{{range .items}}{{.metadata.namespace}} {{.kind}}/{{.metadata.name}}{{"\n"}}{{end}}' | fzf | awk '{print tolower($0)}')
+  #kubectl describe --namespace $(kubectl get all --all-namespaces --template '{{range .items}}{{.metadata.namespace}} {{.kind}}/{{.metadata.name}}{{"\n"}}{{end}}' ) | \
+  kubectl api-resources --verbs=list --namespaced -o name | grep -v "events.events.k8s.io" | grep -v "events" | sort -u | \
+    fzf --preview 'kubectl get {} -o yaml' \
+        --bind "ctrl-\:execute(kubectl get {+} -o yaml | nvim )" \
+        --bind "ctrl-r:reload(kubectl get $* -o name)" --header 'Press CTRL-R to reload' \
+        --bind "ctrl-]:execute(kubectl edit {+})";
+}
+
+kubectlgetall() {
+  for i in $(kubectl api-resources --verbs=list --namespaced -o name | grep -v "events.events.k8s.io" | grep -v "events" | sort | uniq); do
+    echo "Resource:" $i
+    kubectl -n ${1} get --ignore-not-found ${i}
+  done
+}
