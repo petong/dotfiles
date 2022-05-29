8b36a1eb8a369430b4760d18c9ebfaa4c574281f
Pete Hicks <jph@jah.net>
2022-05-29 08:30:05 -0700

    whitespaces cleanup

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -50,7 +50,7 @@ hs.hotkey.bind(shift_hyper, '1', function()
   local win = hs.window.focusedWindow()
   -- get the screen where the focused window is displayed, a.k.a. current screen
   local screen = win:screen()
-  -- compute the unitRect of the focused window relative to the current screen -- and move the window to the next screen setting the same unitRect 
+  -- compute the unitRect of the focused window relative to the current screen -- and move the window to the next screen setting the same unitRect
   win:move(win:frame():toUnitRect(screen:frame()), screen:next(), true, 0)
 end)
 
