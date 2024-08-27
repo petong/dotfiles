2aa084be298434fb5272b9ecea64bfcb80a5f096
Peter Hicks <phicks@ripple.com>
2024-08-27 10:59:06 -0700

    keybinding changes for atuin

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -3,10 +3,10 @@ bindkey -e
 bindkey ' ' magic-space    # also do history expansion on space
 bindkey '^I' complete-word # complete on tab, leave expansion to _expand
 
-# bindkey "^[[A" history-beginning-search-backward
-bindkey "^[[A" atuin-up-search
+bindkey "^[[A" history-beginning-search-backward
+# bindkey "^[[A" atuin-up-search
 bindkey "^[[B" history-beginning-search-forward
-# bindkey "^[OA" history-beginning-search-backward
-bindkey "^[OA" atuin-up-search
+bindkey "^[OA" history-beginning-search-backward
+# bindkey "^[OA" atuin-up-search
 bindkey "^[OB" history-beginning-search-forward
 
